362a3f7756a4471e31a07cf9b4e67b792740a1f7
sebastiangalvan771-hub <sebastiangalvan771@gmail.com>
2026-09-15 17:55:52 +0000

    update

--- Java/101_Lecture_Notes/LectureInput.java
+++ Java/101_Lecture_Notes/LectureInput.java
@@ -1,9 +1,23 @@
 /*
     Lecture note example - Input!!
 */
-
+import java.util.Scanner;
 class LectureInput{
     public static void main(String args[]) {
-        // Your Code Goes here!
+    Scanner sc = new Scanner(System.in);
+    System.out.println("🌮Welcome to Taco Loco!🌮");
+    System.out.println("Here's our menu:");
+    double TCSPrice = 2.39;
+    double TPrice = 3.65;
+    double PCFPrice = 4.12;
+    double JPrice = 2.14;
+    System.out.println("1. Tacos con Salsa - $"+TCSPrice);
+    System.out.println("2. Tamales - $"+TPrice);
+    System.out.println("3. Polo con Frijoles - $"+PCFPrice);
+    System.out.println("4. Jarritos - $"+JPrice);
+
+
+    System.out.println("How much Tacos con Salsa would you like");
+    Scanner sc=new Scanner()
 	}
 }
