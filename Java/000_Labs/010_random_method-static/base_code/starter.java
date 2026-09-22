ed194f3f8e9c5c6ea64ed13a097746b6654c69ed
sebastiangalvan771-hub <sebastiangalvan771@gmail.com>
2026-09-22 17:56:03 +0000

    update

--- Java/000_Labs/010_random_method-static/base_code/starter.java
+++ Java/000_Labs/010_random_method-static/base_code/starter.java
@@ -12,7 +12,7 @@ class starter {
 		// the command window when you compile and run this program.
 		System.out.println("A number between 0 - 9: "+(int)(Math.random()*9));
 		System.out.println("A number between 1 - 10: "+(int)(Math.random()*10));
-		System.out.println("A number between 2.5 and 3.5: "+(Math.random()/2.5*3.5));
+		System.out.println("A number between 2.5 and 3.5: "+(Math.random()*3.5+2.5));
 		System.out.print("A double between 14 and 589: "+(int)(Math.random()-14+589));
 
 	}
--- Java/102_Exercises/10_RandomRange/base_code/starter.java
+++ Java/102_Exercises/10_RandomRange/base_code/starter.java
@@ -10,6 +10,19 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter 2 numbers to create a range for your random number.");
+		System.out.println("Please enter an integer: ");
+		int x = sc.nextInt();
+		System.out.println("Please enter another integer (bigger than the first):" );
+		int y = sc.nextInt();
+		int a = (int)(Math.random()*(y-x)+x);
+		int f = (int)(Math.random()*(y-x)+x);
+		int z = (int)(Math.random()*(y-x)+x);
+		int c = (int)(Math.random()*(y-x)+x);
+		int h = (int)(Math.random()*(y-x)+x);
+		System.out.println("Your range is "+x+ " to " +y+".");
+		System.out.println("Here are 5 numbers generated in that range.");
+		System.out.print(a+", "+f+", "+z+", "+c+", "+h);
 	}
 }
