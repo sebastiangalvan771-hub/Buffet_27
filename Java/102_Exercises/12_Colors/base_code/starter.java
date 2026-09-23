d6fe1e80d878cd7fe3ec087f7a381831bacb69ef
sebastiangalvan771-hub <sebastiangalvan771@gmail.com>
2026-09-23 17:52:40 +0000

    update

--- Java/102_Exercises/12_Colors/base_code/starter.java
+++ Java/102_Exercises/12_Colors/base_code/starter.java
@@ -8,8 +8,25 @@ import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		
-
+		int r = (int)(Math.random()*256);
+        int b = (int)(Math.random()*256);
+        int g = (int)(Math.random()*256);
+        System.out.println("Random color: rgb("+r+", "+g+", "+b+")");
+        int compr = 255-r;
+        int compg = 255-g;
+        int compb = 255-b;
+        System.out.println("Complementary Colors: rgb("+r+", "+g+", "+b+")");
+        getColor(r, g, b);
+        getColor(compr, compg, compb);
+        System.out.println("Triadic Colors:");
+        getColor(r, g, b);
+        getColor(b, r, g);
+        getColor(g, b, r);
+        int darkr = (int)(Math.random()*129);
+        int darkb = (int)(Math.random()*129);
+        int darkg = (int)(Math.random()*129);
+        System.out.println("Darker color:");
+        getColor(darkr, darkg, darkb);
 		// Call getColor(#, #, #);
 	}
 
